a8b19c2cea871070113945f8d69186da7360d535
Jonathan Kwinta <jkwinta@Jonathans-MacBook-Air-2.local>
2017-11-10 00:21:51 -0500

    New FretButton class, nothing useful yet

---
+++ src/fretboard/FretButton.java
@@ -0,0 +1,33 @@
+package fretboard;
+
+import javax.swing.Action;
+import javax.swing.Icon;
+import javax.swing.JButton;
+
+public class FretButton extends JButton {
+	
+//	public FretButton() {
+//		// TODO Auto-generated constructor stub
+//	}
+//
+//	public FretButton(Icon icon) {
+//		super(icon);
+//		// TODO Auto-generated constructor stub
+//	}
+//
+//	public FretButton(String text) {
+//		super(text);
+//		// TODO Auto-generated constructor stub
+//	}
+//
+//	public FretButton(Action a) {
+//		super(a);
+//		// TODO Auto-generated constructor stub
+//	}
+//
+//	public FretButton(String text, Icon icon) {
+//		super(text, icon);
+//		// TODO Auto-generated constructor stub
+//	}
+
+}

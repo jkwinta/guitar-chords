b8d9566dad64c7d8025b50781086d582a1753237
Jonathan Kwinta <jkwinta@Jonathans-MacBook-Air-2.local>
2017-11-17 17:22:45 -0500

    Started working on getting dots in the right place

--- src/fretboard/FretboardLayoutFactory.java
+++ src/fretboard/FretboardLayoutFactory.java
@@ -1,5 +1,7 @@
 package fretboard;
 
+import java.util.HashMap;
+
 import fretboard.FretButton.FretButtonType;
 
 public class FretboardLayoutFactory {
@@ -8,12 +10,48 @@ public class FretboardLayoutFactory {
 		// TODO Auto-generated constructor stub
 	}
 
-	enum FretboardStyle {
+	/**
+	 * Possible included fretboard dot decoration configurations.
+	 */
+	static enum FretboardStyle {
 		STANDARD
 	};
 
-	FretButtonType[][] getLayout(int numberOfStrings) {
-		return null;
+	static HashMap<Integer, Integer> a;
+
+	static FretButtonType[][] getLayout(int numberOfStrings, int numberOfFrets, FretboardStyle style) {
+		if (style == FretboardStyle.STANDARD) {
+			return getStandardLayout(numberOfStrings, numberOfFrets);
+		}
+		FretButtonType[][] layout = new FretButtonType[numberOfFrets][numberOfStrings];
+		return layout;
+	}
+
+	private static FretButtonType[][] getStandardLayout(int numberOfStrings, int numberOfFrets) {
+		FretButtonType[][] layout = new FretButtonType[numberOfFrets][numberOfStrings];
+		layout[0] = getBarRow(numberOfStrings);
+		return layout;
+	}
+
+	private static FretButtonType[] getBarRow(int numberOfStrings) {
+		FretButtonType[] row = new FretButtonType[numberOfStrings];
+		for (int i = 0; i < numberOfStrings; i++) {
+			row[i] = FretButtonType.BAR;
+		}
+		return row;
+	}
+
+	private static int getNumberOfDots(int fretNumber, FretboardStyle style) {
+		if (style == FretboardStyle.STANDARD) {
+			if (fretNumber % 12 == 0) {
+				return 2;
+			} else if (fretNumber % 2 == 1 && (fretNumber + 1) % 12 != 0 && (fretNumber - 1) % 12 != 0) {
+				return 1;
+			} else {
+				return 0;
+			}
+		}
+		return 0;
 	}
 
 }

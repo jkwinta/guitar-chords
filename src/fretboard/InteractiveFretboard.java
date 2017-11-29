6be8b39958d29fa241a7b59654925e453d58ea54
Jonathan Kwinta <jkwinta@Jonathans-MacBook-Air-2.local>
2017-11-28 23:29:53 -0500

    Construct fretboard from layout started, not finished,

--- src/fretboard/InteractiveFretboard.java
+++ src/fretboard/InteractiveFretboard.java
@@ -5,6 +5,7 @@ import java.awt.HeadlessException;
 
 import javax.swing.JFrame;
 
+import fretboard.FretboardLayout.FretboardStyle;
 
 /**
  * The JFrame that handles building the interactive Fretboard.
@@ -13,11 +14,11 @@ import javax.swing.JFrame;
  *
  */
 @SuppressWarnings("serial") // Not serializing, quite yet
-public class Fretboard extends JFrame {
+public class InteractiveFretboard extends JFrame {
 
 	/**
 	 * The gap between components in the GridLayout. Used for both horizontal
-	 * and vertical gap values
+	 * and vertical gap values. This should probably stay 0, as is.
 	 */
 	final static int GRID_LAYOUT_GAP = 0;
 
@@ -30,37 +31,55 @@ public class Fretboard extends JFrame {
 	/** Array of FretObservers each representing instrument strings. */
 	private FretHandler[] strings;
 
+	/** The FretboardLayout prescribing the Icons to use for button styles. */
+	private FretboardLayout layout;
+
 	/**
-	 * Constructor for this Fretboard object.
+	 * Constructor with no arguments uses defaults for number of strings and
+	 * number of frets.
 	 * 
-	 * @param numberOfStrings
-	 * @param numberOfFrets
 	 * @throws HeadlessException
 	 */
-	Fretboard(int numberOfStrings, int numberOfFrets) throws HeadlessException {
+	InteractiveFretboard() throws HeadlessException {
+		this(DEFAULT_NUMBER_OF_STRINGS, DEFAULT_NUMBER_OF_FRETS);
+	}
+
+	InteractiveFretboard(int numberOfStrings, int numberOfFrets) throws HeadlessException {
+		this(new FretboardLayout(numberOfStrings, numberOfFrets, FretboardStyle.STANDARD));
+	}
+
+	InteractiveFretboard(FretboardLayout layout) {
 		super("Interactive fretboard");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.layout = layout;
+		int numberOfStrings = layout.getNumberOfStrings();
+		int numberOfFrets = layout.getNumberOfFrets();
 		this.setLayout(new GridLayout(numberOfFrets + 2, numberOfStrings, GRID_LAYOUT_GAP, GRID_LAYOUT_GAP));
 		// Assemble the Fretboard:
 		this.strings = new FretHandler[numberOfStrings];
-		FretDisplayLabel newLabel;
+		FretValueDisplayLabel newLabel;
 		// Add labels at top:
 		for (int i = 0; i < numberOfStrings; i++) {
-			newLabel = new FretDisplayLabel();
+			newLabel = new FretValueDisplayLabel();
 			this.strings[i] = new FretHandler(numberOfFrets);
 			this.strings[i].addObserverAndUpdate(newLabel);
 			this.add(newLabel);
 		}
 		// Add buttons:
+		
+		// TODO: HERE
+		// HERE
+		// Too much egg nog
+		
 		FretButton newButton;
 		int fretNumber, stringNumber, numberOfButtons;
 		numberOfButtons = numberOfStrings * (numberOfFrets + 1);
 		for (int i = 0; i < numberOfButtons; i++) {
 			fretNumber = i / numberOfStrings;
 			stringNumber = i % numberOfStrings;
-			if (fretNumber == 0){
+			if (fretNumber == 0) {
 				newButton = new FretButton(fretNumber, this.strings[stringNumber], FretButton.FretButtonType.BAR);
-			} else{
+			} else {
 				newButton = new FretButton(fretNumber, this.strings[stringNumber], FretButton.FretButtonType.REGULAR);
 			}
 			this.strings[stringNumber].addFretButton(newButton);
@@ -69,13 +88,10 @@ public class Fretboard extends JFrame {
 		// Finish and show frame:
 		this.pack();
 		this.setVisible(true);
-	}
 
-	Fretboard() throws HeadlessException {
-		this(DEFAULT_NUMBER_OF_STRINGS, DEFAULT_NUMBER_OF_FRETS);
 	}
 
 	public static void main(String[] args) {
-		new Fretboard();
+		new InteractiveFretboard();
 	}
 }

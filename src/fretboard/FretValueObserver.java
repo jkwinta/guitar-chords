f396b3a8add3acc8d708dd39ad91733db17e3f00
Jonathan Kwinta <jkwinta@Jonathans-MacBook-Air-2.local>
2017-11-30 18:05:28 -0500

    Simply an explanation: We should be observing all strings/fretHandlers together, not using the observer/observable because it exists. This will replace FretValDisplayLabel

---
+++ src/fretboard/FretValueObserver.java
@@ -0,0 +1,17 @@
+package fretboard;
+
+/**
+ * A class to handle storing the values of strings on the fretboard, mimicking
+ * an observer pattern, but tailored better to the fact that we change (and thus
+ * update) only one string at a time.
+ * 
+ * @author jkwinta
+ *
+ */
+public class FretValueObserver {
+
+	public FretValueObserver() {
+		// TODO Auto-generated constructor stub
+	}
+
+}

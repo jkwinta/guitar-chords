28cd6e92d99bd9ab51b2dadf3090c931ac3ada55
Jonathan Kwinta <jkwinta@Jonathans-MacBook-Air-2.local>
2017-12-03 13:05:42 -0500

    same as the last

---
+++ src/guitarchords/FrettingChooser.java
@@ -0,0 +1,16 @@
+package guitarchords;
+
+import javax.swing.JFrame;
+
+@SuppressWarnings("serial") // No serializing, yet
+public abstract class FrettingChooser extends JFrame {
+
+	public FrettingChooser() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public FrettingChooser(String text) {
+		super(text);
+	}
+
+}

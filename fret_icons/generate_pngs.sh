7e5a60216c869d55943ba06b6bc09a331dbd0155
Jonathan Kwinta <jkwinta@gmail.com>
2018-07-22 23:21:17 -0400

    indent for clarity

--- fret_icons/generate_pngs.sh
+++ fret_icons/generate_pngs.sh
@@ -5,6 +5,6 @@ HEIGHT=32
 
 for file in *.svg
 do
-# echo $file  png/${file/%.svg/.png}
-inkscape -z $file -e  png/${file/%.svg/.png} -w $WIDTH -h $HEIGHT
+    # echo $file  png/${file/%.svg/.png}
+    inkscape -z $file -e  png/${file/%.svg/.png} -w $WIDTH -h $HEIGHT
 done

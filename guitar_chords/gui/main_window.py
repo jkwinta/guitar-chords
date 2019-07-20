1cd93288b78ae444a464a2eff7285c0a3c845acf
Jonathan Kwinta <jkwinta@gmail.com>
2019-07-20 01:09:41 -0400

    Go button opens or refreshes fretboard

--- guitar_chords/gui/main_window.py
+++ guitar_chords/gui/main_window.py
@@ -2,7 +2,8 @@ import tkinter as tk
 
 from guitar_chords.notes import NOTES
 from .select_root_note_frame import SelectRootNoteFrame
-from .note_collection_select_frame import NoteCollectionSelectFrame
+from .select_note_collection_frame import SelectNoteCollectionFrame
+from .fretboard import Fretboard
 
 
 class MainWindow(tk.Frame):
@@ -11,18 +12,24 @@ class MainWindow(tk.Frame):
         # Add root note select:
         self.root_select = SelectRootNoteFrame(self)
         self.root_select.pack()
-        self.note_collection_select = NoteCollectionSelectFrame(self)
+        self.note_collection_select = SelectNoteCollectionFrame(self)
         self.note_collection_select.pack(fill=tk.X)
         self.go_button = tk.Button(self, text='Go!',
                                    command=self.go_button_callback)
         self.go_button.pack()
         self.pack()
+        self.fretboard = None
 
     def go_button_callback(self):
         print(self.root_select.get_selected_note(),
               self.note_collection_select.get_selected_note_collection_name(),
               self.note_collection_select.get_selected_note_collection(),
               flush=True)
+
+        if self.fretboard is None or not self.fretboard.winfo_exists():
+            self.fretboard = Fretboard(self, None, None, None)
+        else:
+            self.fretboard.update_fretboard(None, None, None)
 
 
 if __name__ == '__main__':

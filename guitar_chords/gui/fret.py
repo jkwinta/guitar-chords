f58a50ba4bca714cd41bf438f35c01b45544dc41
Jonathan Kwinta <jkwinta@gmail.com>
2019-07-20 01:08:14 -0400

    frame to show fret icon

--- guitar_chords/gui/fret.py
+++ guitar_chords/gui/fret.py
@@ -1,5 +1,38 @@
 import tkinter as tk
+from PIL import Image, ImageTk
+from os import path
+
+IMAGE_DIR = './files/fret_icons/png'
+
+IMAGE_PATHS_BY_DECORATOR = {
+    'bar': ['bar', 'bar_fretted'],
+    'left': ['left', 'left_fretted'],
+    'right': ['right', 'right_fretted'],
+    'reg': ['reg', 'reg_fretted'],
+}
+
+PHOTO_IMAGES_BY_DECORATOR = {
+    deco: [None for _ in ims] for deco, ims in IMAGE_PATHS_BY_DECORATOR.items()
+}
 
 
-class Fret(tk.Frame):
-    pass
+def get_photo_image(decoration, status):
+    if PHOTO_IMAGES_BY_DECORATOR[decoration][bool(status)] is None:
+        image_name = IMAGE_PATHS_BY_DECORATOR[decoration][bool(status)]
+        image_path = path.join(IMAGE_DIR, image_name + '.png')
+        result = ImageTk.PhotoImage(Image.open(image_path))
+        PHOTO_IMAGES_BY_DECORATOR[decoration][bool(status)] = result
+    return PHOTO_IMAGES_BY_DECORATOR[decoration][bool(status)]
+
+
+# ImageTk.PhotoImage(Image.open(path.join(IMAGE_DIR, im + '.png')))
+
+
+class Fret(tk.Label):
+    def __init__(self, master, decoration):
+        super().__init__(master)
+        if decoration in IMAGE_PATHS_BY_DECORATOR:
+            self.decoration = decoration
+        else:
+            self.decoration = 'reg'
+        self.config(image=get_photo_image(self.decoration, 0), borderwidth=0)

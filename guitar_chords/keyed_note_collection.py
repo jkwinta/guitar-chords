eb927baed3450a1daf6ab0c39da22215d458d5dc
Jonathan Kwinta <jkwinta@gmail.com>
2019-10-21 22:24:07 -0400

    Get chords contained in keyed note collection

--- guitar_chords/keyed_note_collection.py
+++ guitar_chords/keyed_note_collection.py
@@ -1,6 +1,6 @@
 from guitar_chords.chords import chords
 from guitar_chords.scales import scales
-from guitar_chords.notes import note_name_to_index
+from guitar_chords.notes import note_name_to_index, degree_note_name
 from guitar_chords.relative_note_collection import RelativeNoteCollection
 
 
@@ -17,7 +17,7 @@ class KeyedNoteCollection:
         self.scales_containing = None
 
     def __str__(self):
-        return ' '.join(w.captialize() for w in (
+        return ' '.join(w.capitalize() for w in (
                 [self.root_name]
                 + self.collection_name.split(' ')
                 + [self.scale_or_chord])
@@ -34,10 +34,22 @@ class KeyedNoteCollection:
     def get_chords_contained(self):
         if self.chords_contained is None:
             self.chords_contained = []
-            #
+            for degree_name in self.relative_note_collection.get_note_names():
+                degree_root_note = degree_note_name(self.root_name, degree_name)
+                for chord_name in chords:
+                    chord = KeyedNoteCollection(degree_root_note, 'chord',
+                                                chord_name)
+                    if set(chord.get_note_values()) <= set(
+                            self.get_note_values()):
+                        self.chords_contained.append(chord)
         return self.chords_contained
 
     def get_scales_containing(self):
         if self.scales_containing is None:
             pass
         return self.scales_containing
+
+
+if __name__ == '__main__':
+    a = KeyedNoteCollection('C', 'chord', 'MAJOR')
+    print(a)

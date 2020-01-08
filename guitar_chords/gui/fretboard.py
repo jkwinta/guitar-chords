1083e4f0bf2976449eb4ee22660ee7b77b21e344
jkwinta <jkwinta@gmail.com>
2020-01-07 22:52:08 -0500

    Set a title of what's being displayed

--- guitar_chords/gui/fretboard.py
+++ guitar_chords/gui/fretboard.py
@@ -13,33 +13,14 @@ class Fretboard(tk.Toplevel):
                  orientation, number_of_frets=21, number_of_strings=6):
         super().__init__(master)
         #
-        self.tuning_name = None
         self.keyed_note_collection = None
+        self.tuning_name = None
+        self.tuning = None
         self.orientation = orientation
         self.number_of_frets = number_of_frets
         self.number_of_strings = number_of_strings
-        #
-        # self.tuning = Tuning(tuning_name)
-        self.tuning = None
-        #
-        self.frets = []
 
-        # self.orientation = orientation
-        # self.number_of_frets = number_of_frets
-        # self.number_of_strings = number_of_strings
-        # self.tuning_name = None
-        # self.tuning_notes = ()
-        # self.tuning_values = ()
-        # self.keyed_note_collection = keyed_note_collection
-        # self.root_name = None
-        # self.root_value = None
-        # self.note_collection_name = None
-        # self.note_collection = []
-        # self.note_collection_degrees = []
-        # self.note_collection_values = []
-        # self.notes = set()
-        #
-        # self.frets = {}
+        self.frets = []
 
         self.fretboard_frame = FretboardFrame(self)
         fret_decorator = FretDecorator(self.number_of_frets,
@@ -58,36 +39,11 @@ class Fretboard(tk.Toplevel):
         self.fretboard_frame.pack()
         self.update_fretboard(tuning_name, keyed_note_collection)
 
-    # def update_attributes(self):
-    #     """Called after setting tuning_name, root_name, and note_collection_name
-    #     to update other attributes.
-    #     """
-    #     self.tuning_notes = tunings[self.tuning_name]
-    #     self.tuning_values = tuple(note_name_to_index(n)
-    #                                for n in self.tuning_notes)
-    #
-    #     self.root_value = note_name_to_index(self.root_name)
-    #
-    #     if self.note_collection_name[0] == 'scale':
-    #         self.note_collection = scales[self.note_collection_name[1]]
-    #     elif self.note_collection_name[0] == 'chord':
-    #         self.note_collection = chords[self.note_collection_name[1]]
-    #     else:
-    #         self.note_collection = []
-    #
-    #     self.note_collection_degrees = [
-    #         int('0' + ''.join(c for c in n if c.isdigit()))
-    #         for n in self.note_collection]
-    #     self.note_collection_values = [
-    #         names_to_semitones[n] for n in self.note_collection]
-    #
-    #     self.notes = {(n + self.root_value) % 12
-    #                   for n in self.note_collection_values}
-
     def update_fretboard(self, tuning_name, keyed_note_collection):
         if (self.tuning_name, self.keyed_note_collection) == (
                 tuning_name, keyed_note_collection):
             return
+        self.title(keyed_note_collection)
         # Set all attr declared in init:
         self.keyed_note_collection = keyed_note_collection
         self.tuning_name = tuning_name
@@ -103,14 +59,12 @@ class Fretboard(tk.Toplevel):
             else:
                 fret.set_unfretted()
 
-    # def set_fretting(self):
-    #     pass  # TODO
-
     def clear_fretboard(self):
         for fret_content in self.frets:
             fret_content['fret'].set_unfretted()
 
 
+# Simply to have the whole thing be in a Frame within the Toplevel
 class FretboardFrame(tk.Frame):
     def __init__(self, master):
         super().__init__(master)
